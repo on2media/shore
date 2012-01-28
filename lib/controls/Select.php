22ee4e4de9233bfc15b844ba71cd463f1378d1fa
Lee Hesselden <on2@users.noreply.github.com>
2012-01-28 18:05:52 +0000

    Changed: A default option can be setup by extending the Select control.

--- lib/controls/Select.php
+++ lib/controls/Select.php
@@ -18,6 +18,10 @@ class SelectControl extends Control
         
         $field = "";
         
+        $selected = FALSE;
+        if ($this->_obj->$func() instanceof $this->_objType) $selected = $this->_obj->$func()->uid();
+        if (!$selected) $selected = $this->getDefaultSelected();
+        
         if ($options->fetchAll()->count() > 0) {
             
             $field = sprintf("<select name=\"%s\">\n    <option value=\"0\">&nbsp;</option>", $this->_prefix . $this->_var);
@@ -25,7 +29,7 @@ class SelectControl extends Control
             foreach ($options as $option) {
                 $field .= sprintf("    <option value=\"%s\"%s>%s</option>\n",
                     $option->uid(),
-                    ($this->_obj->$func() instanceof $this->_objType && $this->_obj->$func()->uid() == $option->uid() ? " selected=\"selected\"":""),
+                    ($selected == $option->uid() ? " selected=\"selected\"":""),
                     $option->cite()
                 );
             }
@@ -75,5 +79,13 @@ class SelectControl extends Control
         $optionClass = $this->_objType;
         $options = new $optionClass();
         return $options->getCollection();
+    }
+    
+    /**
+     *
+     */
+    public function getDefaultSelected()
+    {
+        return FALSE;
     }
 }
